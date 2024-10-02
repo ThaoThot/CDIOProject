66d4845fdde0503bc9a9e77f1ddb9e7cb2c14433
duc-oizz25 <hoangtrungduc2552002@gmail.com>
2024-10-03 02:04:20 +0700

    ai for Bot
    lỗi

---
+++ Assets/Script/Enemy_Behaviour.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Behavior : MonoBehaviour
+{
+    public Transform rayCast;
+    public LayerMask rayCastMask;
+    public float rayCastLength;
+    public float attackDistance;    // Khoảng cách tối thiểu để tấn công
+    public float moveSpeed;
+    public float timer;     // Thời gian hồi chiêu
+
+    private RaycastHit2D hit;
+    private GameObject target;
+    private Animator anim;
+    private float distance;
+    private bool attackMode;
+    private bool inRange;
+    private bool cooling;
+    private float intTimer;
+
+
+    void Awake()
+    {
+        intTimer = timer;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (inRange)
+        {
+            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, rayCastMask);
+            RayCastDebugger();
+        }
+        // Khi phát hiện người chơi
+        if (hit.collider != null)
+        {
+            EnemyLogic();
+        }
+        else if (hit.collider == null)
+        {
+            inRange = false;
+        }
+
+        if (inRange == false)
+        {
+            anim.SetBool("CanWalk", false);
+            StopAttack();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            target = other.gameObject;
+            inRange = true;
+        }
+    }
+
+    void EnemyLogic()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+
+        if (distance > attackDistance)
+        {
+            Move();
+            StopAttack();
+        }
+        else if (attackDistance >= distance && cooling == false)
+        {
+            Attack();
+        }
+
+        if (cooling)
+        {
+            anim.SetBool("Attack", false);
+        }
+    }
+
+    void Move()
+    {
+        anim.SetBool("CanWalk", true);
+
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Skel_attack"))
+        {
+            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void Attack()
+    {
+        timer = intTimer;
+        attackMode = true;
+
+        anim.SetBool("CanWalk", false);
+        anim.SetBool("Attack", true);
+    }
+
+    void StopAttack()
+    {
+        cooling = false;
+        attackMode = false;
+        anim.SetBool("Attack", false);
+    }
+
+    void RayCastDebugger()
+    {
+        if (distance > attackDistance)
+        {
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+        }
+        else if (attackDistance > distance)
+        {
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.green);
+        }
+    }
+}
